e291d8a24216d8acf3b120c900234c06bfb449f4
SabahSayed03 <sayedsabah41@gmail.com>
2025-11-16 18:40:10 +0530

    Fix MySQL DB connection

--- backend/db.js
+++ backend/db.js
@@ -3,12 +3,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = mysql.createPool(process.env.DB_URL);
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 pool.getConnection()
   .then((conn) => {
     console.log("Connected to MySQL database!");
-    conn.release(); // release connection back to pool
+    conn.release();
   })
   .catch((err) => {
     console.error("Database connection failed:", err.message);
